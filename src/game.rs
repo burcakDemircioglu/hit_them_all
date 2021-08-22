21910be06f4e6df26b2f1cadbaea20b4d7bf0d97
Burcak Kam <budemirc@microsoft.com>
2021-08-22 23:01:53 +0300

    Create the invaders

--- src/game.rs
+++ src/game.rs
@@ -1,17 +1,20 @@
+use std::convert::TryInto;
+
 use ggez::{
     self, event,
     graphics::{self, Color, DrawMode, DrawParam, Mesh},
     input::keyboard::{self, KeyCode},
     nalgebra as na, Context, GameResult,
 };
+use rand::{thread_rng, Rng};
 
 mod constants;
 mod utilities;
 
 pub struct GameState {
     player_pos: na::Point2<f32>,
-    invader_pos: std::collections::HashSet<na::Point2<f32>>,
-    fire_pos: std::collections::HashSet<na::Point2<f32>>,
+    invader_positions: std::vec::Vec<na::Point2<f32>>,
+    fire_pos: std::vec::Vec<na::Point2<f32>>,
     score: i32,
     life: i32,
 }
@@ -20,14 +23,24 @@ impl GameState {
     pub fn new(context: &mut Context) -> Self {
         let (screend_width, screen_hight) = graphics::drawable_size(context);
         let (screend_width_half, screen_hight_half) = (screend_width * 0.5, screen_hight * 0.5);
+        let mut rng = thread_rng();
+
+        let mut invaders = std::vec::Vec::<na::Point2<f32>>::new();
+
+        for i in 0..constants::INVADER_AMOUNT {
+            invaders.push(na::Point2::<f32>::new(
+                rng.gen_range(0.0, screend_width - constants::INVADER_SIZE),
+                rng.gen_range(-1000.0, 0.0),
+            ));
+        }
 
         GameState {
             player_pos: na::Point2::new(
                 screend_width_half - constants::PLAYER_WIDTH_HALF,
                 screen_hight - constants::PLAYER_PADDING,
             ),
-            invader_pos: std::collections::HashSet::new(),
-            fire_pos: std::collections::HashSet::new(),
+            invader_positions: invaders,
+            fire_pos: std::vec::Vec::new(),
             score: 0,
             life: 3,
         }
@@ -37,7 +50,20 @@ impl GameState {
 impl event::EventHandler for GameState {
     fn update(&mut self, context: &mut Context) -> GameResult {
         let dt = ggez::timer::delta(context).as_secs_f32();
-        let (screend_width, screen_hight) = graphics::drawable_size(context);
+        let (screend_width, screen_height) = graphics::drawable_size(context);
+        let mut rng = thread_rng();
+
+        for invader_pos in &mut self.invader_positions {
+            invader_pos.y += constants::INVADER_SPEED * dt;
+
+            if invader_pos.y > screen_height {
+                *invader_pos = na::Point2::new(
+                    rng.gen_range(0.0, screend_width - constants::INVADER_SIZE),
+                    0.0,
+                );
+                self.life -= 1;
+            }
+        }
 
         if keyboard::is_key_pressed(context, KeyCode::Right) {
             self.player_pos.x += constants::PLAYER_SPEED * dt;
@@ -73,6 +99,19 @@ impl event::EventHandler for GameState {
         );
         let player_mesh = Mesh::new_rectangle(context, DrawMode::fill(), player, graphics::WHITE)?;
         graphics::draw(context, &player_mesh, DrawParam::default())?;
+
+        // Draw invaders
+        for invader_pos in self.invader_positions.iter() {
+            let invader = graphics::Rect::new(
+                invader_pos.x,
+                invader_pos.y,
+                constants::INVADER_SIZE,
+                constants::INVADER_SIZE,
+            );
+            let invader_mesh =
+                Mesh::new_rectangle(context, DrawMode::fill(), invader, graphics::WHITE)?;
+            graphics::draw(context, &invader_mesh, DrawParam::default())?;
+        }
 
         graphics::present(context)?;
         Ok(())
--- src/game/constants.rs
+++ src/game/constants.rs
@@ -4,5 +4,10 @@ pub const PLAYER_WIDTH_HALF: f32 = PLAYER_WIDTH * 0.5;
 pub const PLAYER_HEIGHT_HALF: f32 = PLAYER_HEIGHT * 0.5;
 pub const PLAYER_PADDING: f32 = PLAYER_HEIGHT*2.0;
 pub const PLAYER_SPEED: f32 = 300.0;
+pub const INVADER_SPEED: f32 = 200.0;
+pub const INVADER_AMOUNT: i32 = 5;
+pub const INVADER_SIZE: f32 = 10.0;
+
+
 
 
